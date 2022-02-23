28dbb705ed7f0b5d0f696f8b249f76178ef933df
marissann11 <wilhite.marissa@gmail.com>
2022-02-23 01:30:01 -0500

    helper functions and handlebars partials

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,15 +60,15 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-  .then(dbUserData => {
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-  
-      res.json(dbUserData);
-    });
-  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -76,6 +76,7 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
@@ -94,7 +95,6 @@ router.post('/login', (req, res) => {
     }
 
     req.session.save(() => {
-      // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
@@ -109,8 +109,7 @@ router.post('/logout', (req, res) => {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  }
-  else {
+  } else {
     res.status(404).end();
   }
 });
@@ -126,7 +125,7 @@ router.put('/:id', (req, res) => {
     }
   })
     .then(dbUserData => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,12 +1,9 @@
 async function commentFormHandler(event) {
   event.preventDefault();
 
-  const comment_text = document
-    .querySelector('textarea[name="comment-body"]')
-    .value.trim();
-
-  const post_id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
+  const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  const post_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
   ];
 
   if (comment_text) {
@@ -20,16 +17,13 @@ async function commentFormHandler(event) {
         'Content-Type': 'application/json'
       }
     });
-  
+
     if (response.ok) {
       document.location.reload();
     } else {
       alert(response.statusText);
     }
   }
-  
 }
 
-document
-  .querySelector(".comment-form")
-  .addEventListener("submit", commentFormHandler);
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
